480e1bd8d2474f264d0a2ebd9aca4358848500b8
LearnerDivesh <learnerdivesh@gmail.com>
2025-03-13 18:20:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ def search_trainers():
     return jsonify({"message": "Trainer search results", "data": matched_trainers})
 
 if __name__ == '__main__':
-    import os  
+    import os
 
-port = int(os.environ.get("PORT", 5000))  # Get PORT from environment variables
-app.run(host="0.0.0.0", port=port, debug=True)  # Bind to 0.0.0.0
+port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+app.run(host="0.0.0.0", port=port, debug=True)
+
 
